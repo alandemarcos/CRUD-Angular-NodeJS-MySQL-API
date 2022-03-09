71a49a0f7c9c9e3404f88956e298623d306cc7c0
Alan Demarcos <67525616+alandemarcos@users.noreply.github.com>
2022-03-08 22:03:31 -0300

    Add files via upload

---
+++ backend/Table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `usuario` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nomecompleto` varchar(100) NOT NULL,
+  `celular` int NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
